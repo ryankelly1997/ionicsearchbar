c73a88cc0a60c709a9d1f6f9ef4652d94c82e992
Ryan Kelly <ryankelly1997@Ryans-MacBook-Pro-2.local>
2020-04-15 16:59:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp({
+    apiKey: "AIzaSyCgEE_TkQCNubUbRnIyzDy74Tcq1baj3s4",
+    authDomain: "testagain-6834c.firebaseapp.com",
+    databaseURL: "https://testagain-6834c.firebaseio.com",
+    projectId: "testagain-6834c",
+    storageBucket: "testagain-6834c.appspot.com",
+    messagingSenderId: "531384045944",
+    appId: "1:531384045944:web:e448f05a93e29ad181788d",
+    measurementId: "G-9XFCLRGH9E"
+  }),
+AngularFirestoreModule],
   providers: [
     StatusBar,
+   
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public goalList: any[];
+  public loadedGoalList: any[];
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.firestore.collection(`goals`).valueChanges()
+      .subscribe(goalList => {
+        this.goalList = goalList;
+        this.loadedGoalList = goalList;
+        console.log('Hell new');
+    });
+  }
+
+  initializeItems(): void {
+    this.goalList = this.loadedGoalList;
+  }
+
+  filterList(evt) {
+    this.initializeItems();
+  
+    const searchTerm = evt.srcElement.value;
+  
+    if (!searchTerm) {
+      return;
+    }
+  
+    this.goalList = this.goalList.filter(currentGoal => {
+      if (currentGoal.goalName && searchTerm) {
+        if (currentGoal.goalName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+  }
 
 }
